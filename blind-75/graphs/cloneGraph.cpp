3f4887d7c967c33532014959ede34b34403769cb
Jimmy <jperez81@itam.mx>
2024-08-11 15:45:03 -0600

    forgotten commit

---
+++ blind-75/graphs/cloneGraph.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+
+Node* cloneGraph(Node* node){
+    queue<Node*> q;
+
+    Node* head = new Node(node->val);
+    for(auto n : node->neighbors) q.push(n);
+
+    return head;
+}
