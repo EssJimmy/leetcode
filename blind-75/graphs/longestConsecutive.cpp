8c291a0c4a43760800b8e3263d61d2ca6639a793
Jaime <jperez81@itam.mx>
2024-09-08 22:20:08 -0600

    back to grind i guess

--- blind-75/graphs/longestConsecutive.cpp
+++ blind-75/graphs/longestConsecutive.cpp
@@ -2,26 +2,51 @@
 
 using namespace std;
 
-/*O(nlogn) solution but we need a better one
+// for some goddamn reason, o(nlogn) is better than o(n)
+class Solution{
+public:
     int longestConsecutive(vector<int>& nums){
-        sort(nums.begin(), nums.end());
+        set<int> s;
 
-        int maxVal = 0; int temp = 0;
-        for(int i = 1; i < (int) nums.size(); i++){
-            if(nums[i] == nums[i-1]+1) ++temp;
+        for(auto i = 0; i < nums.size(); i++) 
+            s.insert(nums[i]);
+        
+        int maxVal = 0, temp = 1;
+        for(auto it = s.begin(); it != s.end(); ++it) {
+            if(s.contains(*it+1)) ++temp;
             else {
                 maxVal = max(maxVal, temp);
-                temp = 0;
+                temp = 1;
             }
         }
 
+        if(nums.size() != 0)
+            maxVal = max(maxVal, temp);
+
         return maxVal;
     }
-*/
+};
 
-class Solution {
+//O(n) solution
+/*class Solution {
 public:
     int longestConsecutive(vector<int>& nums){
+        unordered_set<int> set;
+        int maxVal = 0;
+
+        for(size_t i = 0; i < nums.size(); i++) 
+            set.insert(nums[i]);
         
+        for(size_t i = 0; i < nums.size(); i++) {
+            if(!set.contains(nums[i] - 1)){
+                int temp = 0;
+                while(set.contains(nums[i] + temp)) ++temp;
+
+                maxVal = max(maxVal, temp);
+            }
+            
+        }
+
+        return maxVal;
     }    
-};
+};*/
